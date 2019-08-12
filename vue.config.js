7454e54cbb3312e70e48e1d3ff42c8f5e3bc08ec
lcx <752078351@qq.com>
2019-08-12 10:48:00 +0800

    小修改

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     proxy: {
       '/api': {
         // target: "", //设置你调用的接口域名和端口号 别忘了加http  
-        target: "http://40.00.100.133:3002/user/login",
+        target: "http://40.00.100.133:3002/",
         changeOrigin: true,
         pathRewrite: {
           '^/api': '' //这里理解成用‘/api'代替target里面的地址，后面组件中我们掉接口时直接用api代替
