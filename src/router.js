c0f2255e565c268d7722a95fc06c574ea0c70596
lcx <752078351@qq.com>
2019-08-12 09:49:48 +0800

    修改eslint内容

--- src/router.js
+++ src/router.js
@@ -1,12 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -22,4 +20,4 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
-})
+});
